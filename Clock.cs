31d355278541a59766fb749dd98d669f03936f96
Amelia <91938@student.pb.edu.pl>
2025-06-07 19:08:04 +0200

    mod clock parking program

---
+++ Clock.cs
@@ -0,0 +1,73 @@
+﻿namespace Parking
+{
+    internal class Clock
+    {
+
+        public int Minute;
+        public int Hour;
+        public int Day;
+
+        public Clock()
+        {
+            Minute = 0;
+            Hour = 6;
+            Day = 1;
+
+        }
+
+        public void Tick()
+        {
+            Minute += 15;
+            if (Minute >= 60)
+            {
+                Minute = 0;
+                Hour++;
+                if (Hour >= 22)
+                {
+                    Hour = 6;
+                    Day++;
+                }
+            }
+        }
+        public string DisplayTime()
+        {
+            return $"Dzień {Day} {Hour:D2}:{Minute:D2}";
+        }
+
+        public static Clock operator -(Clock a, Clock b)
+        {
+            int resultA;
+            if (a.Minute > 0) resultA = a.Hour + (a.Day - 1) * 16 + 1;
+            else resultA = a.Hour + (a.Day - 1) * 16;
+            int resultB;
+            if (b.Minute > 0) resultB = b.Hour + (b.Day - 1) * 16 + 1;
+            else resultB = b.Hour + (b.Day - 1) * 16;
+
+            var newClock = new Clock();
+            newClock.Hour = resultA - resultB;
+
+            return newClock;
+        }
+
+        public void SkipToEndOfDay()
+        {
+            while (!(Hour == 21 && Minute == 45))
+            {
+                Tick();
+            }
+        }
+
+        public void SkipToHour(int targetHour)
+        {
+            if (targetHour < Hour || targetHour > 21)
+            {
+                throw new InvalidHourException("Nieprawidłowa godzina - można przesunąć tylko między 6 a 21.");
+            }
+
+            while (Hour < targetHour)
+            {
+                Tick();
+            }
+        }
+    }
+}
---
+++ Parking.cs
@@ -0,0 +1,145 @@
+﻿namespace Parking
+{
+    internal class Parking
+    {
+        public Clock Time { get; private set; }
+        private Dictionary<string, Vehicle> CarList;
+        private readonly int MaximumCapacity;
+        private int ActualCapacity;
+        private Raport Raport;
+
+        public void Tick()
+        {
+            Time.Tick();
+        }
+
+        public Parking()
+        {
+            Time = new Clock();
+            CarList = new Dictionary<string, Vehicle>();
+            MaximumCapacity = 50;
+            ActualCapacity = 0;
+            Raport = new Raport();
+        }
+
+        public void Entrance(Vehicle pojazd)
+        {
+            if (pojazd.IsParked == true)
+            {
+                throw new VehicleAlreadyParkedException("Wybrany pojazd jest już zaparkowany.");
+            }
+            if (pojazd.CarType == CarType.Truck)
+            {
+                if (ActualCapacity + 3 > MaximumCapacity)
+                {
+                    throw new InvalidCapacityException("Parking jest pełny, nie można dodać pojazdu.");
+                }
+                else
+                {
+                    ActualCapacity += 3;
+                    pojazd.IsParked = true;
+                }
+            }
+            else
+            {
+                if (ActualCapacity + 1 > MaximumCapacity)
+                {
+                    throw new InvalidCapacityException("Parking jest pełny, nie można dodać pojazdu.");
+                }
+                else
+                {
+                    ActualCapacity++;
+                    pojazd.IsParked = true;
+                }
+            }
+            if (pojazd.IsParked == true)
+            {
+                pojazd.EntranceTime.Add(new Clock { Day = Time.Day, Hour = Time.Hour, Minute = Time.Minute });
+                CarList[pojazd.Registration] = pojazd;
+                Raport.AddVehicle(Time.Day, pojazd);
+                Time.Tick();
+            }
+        }
+
+        public void Departure(Vehicle pojazd)
+        {
+            if (!pojazd.IsParked || pojazd.EntranceTime.Count == pojazd.DepartureTime.Count)
+            {
+                throw new VehicleAlreadyOutOfParkingException("Nie można wyjechać z parkingu pojazdem, który nie jest na nim zaparkowany.");
+            }
+            pojazd.IsParked = false;
+            pojazd.DepartureTime.Add(new Clock { Day = Time.Day, Hour = Time.Hour, Minute = Time.Minute });
+            CarList.Remove(pojazd.Registration);
+            if (pojazd.CarType == CarType.Truck)
+            {
+                ActualCapacity -= 3;
+            }
+            else
+            {
+                ActualCapacity--;
+            }
+
+            var czasPostoju = pojazd.DepartureTime[^1] - pojazd.EntranceTime[^1];
+
+            if (pojazd.CarType == CarType.PassengerCar || pojazd.CarType == CarType.DeliveryTruck)
+            {
+                int Oplata = 20 * czasPostoju.Hour;
+                Console.WriteLine($"Pojazd {pojazd.Registration} opuścił parking. Opłata za postoj: {Oplata} zl");
+            }
+            else
+            {
+                int OplataT = 60 * czasPostoju.Hour;
+                Console.WriteLine($"Pojazd {pojazd.Registration} opuścił parking. Opłata za postoj: {OplataT} zl");
+            }
+            Console.WriteLine("Wyjazd zarejestrowany.");
+            Time.Tick();
+        }
+
+        public void ParkingCurrentStatus()
+        {
+            Console.WriteLine($"Aktualny czas: {Time.DisplayTime()}");
+            Console.WriteLine($"Dostepne miejsca: {MaximumCapacity-ActualCapacity} na {MaximumCapacity}");
+            Console.WriteLine("Lista pojazdów na parkingu:");
+            foreach (var vehicle in CarList.Values)
+            {
+                Console.WriteLine(vehicle.VehicleInfo());
+            }
+        }
+        public void ShowHistory()
+        {
+            Raport.DisplayHistory();
+        }
+
+        public void SkipToEndOfDay()
+        {
+            Time.SkipToEndOfDay();
+        }
+
+        public void SkipToHour(int hour)
+        {
+            Time.SkipToHour(hour);
+        }
+
+        public void ShowDailyRaport(int day)
+        {
+            Raport.DisplayDailyRaport(day);
+        }
+    }
+
+        internal class InvalidCapacityException : Exception
+        {
+            public InvalidCapacityException(string message) : base(message) { }
+        }
+        internal class VehicleAlreadyParkedException : Exception
+        {
+            public VehicleAlreadyParkedException(string message) : base(message) { }
+        }
+        internal class VehicleAlreadyOutOfParkingException : Exception
+        {
+            public VehicleAlreadyOutOfParkingException(string message) : base(message) { }
+        }
+        internal class InvalidHourException : Exception
+        {
+            public InvalidHourException(string message) : base(message) { }
+        }
+}
